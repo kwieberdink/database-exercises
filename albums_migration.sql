e83b3edc19ceb595a388e907ff9fb222351b1a9a
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-05-01 12:57:21 -0500

    mysql tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    album_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(35) NOT NULL,
+    name VARCHAR(65) NOT NULL,
+    release_date DATE NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(65) NOT NULL,
+    PRIMARY KEY (album_id)
+)
